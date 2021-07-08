e06be9938db916c8b079fa0df794d2f2c1b94ded
FerryAr <38641932+FerryAr@users.noreply.github.com>
2021-07-08 11:34:50 +0700

    Separarte repo

--- forum-diskusi/application/views/layout.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8"/>
-        <meta content="width=device-width, initial-scale=1" name="viewport"/>
-        <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>"/>
-    </head>
-    <body>
